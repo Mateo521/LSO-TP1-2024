9efffade5a2cca8da1740522859c065bff618c0d
Mateo521 <mateovillalva42@gmail.com>
2024-08-16 12:50:27 -0300

    init

--- LSO.h
+++ LSO.h
@@ -1,8 +1,53 @@
 //
 // Created by Usuario on 16/8/2024.
 //
-
+#define MAX_prestadores 110
 #ifndef LSO_TP1_2024_LSO_H
-#define LSO_TP1_2024_LSO_H
+#include "Prestadores.h"
+typedef struct {
+    Prestador prestador [MAX_prestadores];
+    int contador;
+   }LSO;
 
-#endif //LSO_TP1_2024_LSO_H
+int LocalizarLSO(LSO *lista, int *pos, int dni_x) {
+    int i = 0;
+    while (i < lista->contador  && lista->prestador[i].dni < dni_x) {
+        i++;
+    }
+    *pos = i;
+    if (i < lista->contador && lista->prestador[i].dni == dni_x) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+int AltaLSO(LSO *lista, Prestador prestador) {
+    if (lista->contador >= MAX_prestadores) {
+        printf("Error: Limite maximo de prestadores alcanzado.\n");
+        return 2;
+    }
+
+    int pos;
+    int i;
+    int res = LocalizarLSO(lista, &pos, prestador.dni);
+    if (res == 0) {
+        for (i = lista->contador - 1; i >= pos; i--) {
+            lista->prestador[i + 1] = lista->prestador[i];
+        }
+        lista->prestador[pos] = prestador;
+        lista->contador++;
+        printf("Prestador insertado. Nuevo contador: %d\n", lista->contador);
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+
+
+
+
+#define LSO_TP1_2024_LSO_H
+#endif
--- main.c
+++ main.c
@@ -1,26 +1,60 @@
 #include <stdio.h>
 #include "LSO.h"
-#include "Prestadores.h"
+void mostrarestructuraLSO(LSO *lista);
+int Lectura_datos(LSO *lso);
 int main() {
+    LSO lso;
+    lso.contador=  0;
 
+    Lectura_datos(&lso);
 
+   mostrarestructuraLSO(&lso);
    return 0;
 }
-int lectura_datos() {
-    FILE *fp;
+int Lectura_datos(LSO *lso) {
     Prestador aux;
-    if ((fp = fopen("Prestadores.txt", "r")) == NULL)
-        return 0;
-    while (!feof(fp)) {
-        if (fscanf(fp, "%d", &aux.dni) == 1) {
-            fscanf(fp, " %[^\n]", aux.nombre_y_apellido);
-            fscanf(fp, " %[^\n]", aux.servicios);
-            fscanf(fp, " %[^\n]", aux.domicilio);
-            fscanf(fp, " %[^\n]", aux.mail);
-            fscanf(fp, " %[^\n]", aux.telefono);
-            // Procesar los datos del prestador
+    FILE *fp;
+    if ((fp = fopen("C:/ruta/absoluta/Prestadores.txt", "r")) == NULL) {
+        return 0;  // Error al abrir el archivo
+    } else {
+        while (!feof(fp)) {
+            // Leer DNI
+            if (fscanf(fp, "%d\n", &aux.dni) != 1) break;  // Leer DNI y consumir el salto de línea
+            // Leer Nombre y Apellido
+            if (fgets(aux.nombre_y_apellido, sizeof(aux.nombre_y_apellido), fp) == NULL) break;
+            aux.nombre_y_apellido[strcspn(aux.nombre_y_apellido, "\n")] = '\0';
+            // Leer Servicios
+            if (fgets(aux.servicios, sizeof(aux.servicios), fp) == NULL) break;
+            aux.servicios[strcspn(aux.servicios, "\n")] = '\0';
+            // Leer Domicilio
+            if (fgets(aux.domicilio, sizeof(aux.domicilio), fp) == NULL) break;
+            aux.domicilio[strcspn(aux.domicilio, "\n")] = '\0';
+            // Leer Email
+            if (fgets(aux.mail, sizeof(aux.mail), fp) == NULL) break;
+            aux.mail[strcspn(aux.mail, "\n")] = '\0';
+            // Leer Teléfono
+            if (fgets(aux.telefono, sizeof(aux.telefono), fp) == NULL) break;
+            aux.telefono[strcspn(aux.telefono, "\n")] = '\0';
+
+             AltaLSO(lso, aux);
         }
+
+        // Cerrar el archivo
+        fclose(fp);
+        return 1;
     }
-    fclose(fp);
-    return 1;
 }
+
+
+void mostrarestructuraLSO(LSO *lista)
+{
+    int i;
+    for (i = 0; i < lista->contador; i++)
+    {
+        Mostrarprestador(lista->prestador[i]);
+    }
+    printf("Total de %d envios\n", lista->contador);
+}
+
+
+
